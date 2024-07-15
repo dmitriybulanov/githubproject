3ad639433521c50362bbd0032a0785d309446b4b
Dmitry <dimabulan@yandex.ru>
2024-07-15 18:41:55 +0300

    add files

---
+++ main4.py
@@ -0,0 +1,42 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class DBSample(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI1.ui', self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.select_data)
+
+    def select_data(self):
+        # Получим результат запроса,
+        # который ввели в текстовое поле
+        query = 'SELECT * FROM coffee'
+        res = self.connection.cursor().execute(query).fetchall()
+        # Заполним размеры таблицы
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+    def closeEvent(self, event):
+        # При закрытии формы закроем и наше соединение
+        # с базой данных
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = DBSample()
+    ex.show()
+    sys.exit(app.exec())
